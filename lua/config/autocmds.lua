ee07b662f452c1d6e56ad78ebaed1bee5b876b73
Tobias Kaestner <tobias.kaestner@inovex.de>
2026-07-31 17:37:29 +0200

    fix: proper defaults for rst files
    Signed-off-by: Tobias Kaestner <tobias.kaestner@inovex.de>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- ~/.config/nvim/lua/plugins/indent_rst.lua
+-- Sets tabstop, shiftwidth to 3 and expands tabs to spaces for reStructuredText files.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "rst",
+  callback = function()
+    vim.opt_local.tabstop = 3
+    vim.opt_local.shiftwidth = 3
+    vim.opt_local.expandtab = true
+  end,
+})
