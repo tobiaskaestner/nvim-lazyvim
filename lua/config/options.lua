43b041eaf2a4db3dba891dabac80a429eb7b0ac5
Tobias Kaestner <tobias.kaestner@inovex.de>
2026-07-31 17:38:55 +0200

    feat: better cursor
    Signed-off-by: Tobias Kaestner <tobias.kaestner@inovex.de>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.o.guicursor = "n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
+vim.g.root_spec = { { ".west" }, { ".git", "lua", "package.json" }, "lsp", "cwd" }
+vim.g.lazyvim_explorer = "neo-tree"
